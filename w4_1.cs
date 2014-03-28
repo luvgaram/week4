23a94e154261a7e598074bf21d069400058739f0
Garam <garam@nhnnext.org>
2014-03-28 13:51:45 +0900

    first commit

---
+++ w4_1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace w4_1
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.Write ("Enter the 1st no: ");
+			int num1 = int.Parse (Console.ReadLine ());
+			Console.Write ("Enter the 2nd no: ");
+			int num2 = int.Parse (Console.ReadLine ());
+			Console.Write ("Enter the Operation +, -, *, /, %: ");
+			string operation = Console.ReadLine ();
+
+			if(operation == "+"){
+				Console.Write ("{0} + {1} = {2}", num1, num2, num1 + num2);
+			}else if(operation == "-"){
+				Console.Write ("{0} - {1} = {2}", num1, num2, num1 - num2);
+			}else if(operation == "*"){
+				Console.Write ("{0} * {1} = {2}", num1, num2, num1 * num2);
+			}else if(operation == "/"){
+				Console.Write ("{0} / {1} = {2}", num1, num2, num1 / num2);
+			}else if(operation == "%"){
+				Console.Write ("{0} % {1} = {2}", num1, num2, num1 % num2);
+			}else{Console.Write ("You entered invalid operation \"{0}\".", operation);
+			}
+		}
+	}
+}
---
+++ w4_3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Moduler
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.Write ("Enter the 1st number : ");
+			int num1 = int.Parse (Console.ReadLine ()); 
+			Console.Write ("Enter the 2nd number : ");
+			int num2 = int.Parse (Console.ReadLine ());
+			Console.Write ("Enter the operation(/, % are allowed) : ");
+			//Console.Write ("{0} / {1} = {2}, {0} % {1} = {3}", num1, num2, num1 / num2, num1 % num2);
+			// 정수형 나눗셈은 반올림을 안함
+			string operation = Console.ReadLine ();
+			if (operation == "/") {
+				Console.Write ("{0} / {1} = {2}", num1, num2, num1 / num2);
+			}else if (operation == "%") {
+					Console.Write ("{0} % {1} = {2}", num1, num2, num1 % num2);
+			}else{
+				Console.Write ("You entered invalid operation, \"{0}\". Only / and % are allowed.", operation);
+			}
+		}
+	}
+}
